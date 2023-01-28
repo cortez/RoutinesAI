3e3ee7defe047e0d310c1df58da8be83a6717371
cortez <joseph@cortez.link>
2023-01-27 19:16:46 -0600

    Committed with Git-Upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,17 @@
 export default function Footer() {
   return (
     <footer className="text-center w-full mt-8 mb-8 items-center text-white">
-      <div className="text-gray-600">
+      <div>
+        <a
+          href="mailto:joseph@lcortez.com?subject=AI%20Workouts%20Feedback"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white hover:bg-gray-700 py-2 px-3 bg-gray-800 rounded-xl"
+        >
+          Give feedback
+        </a>
+      </div>
+      <div className="text-gray-600 mt-5">
         Powered by{" "}
         <a
           href="https://openai.com/api"
@@ -19,16 +29,6 @@ export default function Footer() {
           className="text-gray-600 hover:text-gray-600"
         >
           Vercel Edge Functions
-        </a>
-      </div>
-      <div className="mt-5">
-        <a
-          href="mailto:joseph@lcortez.com?subject=AI%20Workouts%20Feedback"
-          target="_blank"
-          rel="noreferrer"
-          className="text-white hover:bg-gray-700 py-2 px-3 bg-gray-800 rounded-xl"
-        >
-          Give feedback
         </a>
       </div>
     </footer>
