b830d233a590e835b87b91acab07e609a477b12b
cortez <joseph@cortez.link>
2023-01-28 08:49:36 -0600

    Committed with Git-Upload

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full bg-black text-white font-medium text-md mb-11 flex">
-      <Link href="/" className="top-3 left-4 space-x-3 absolute hover:text-gray-600 transition"
+      <Link href="/" className="top-3 left-4 space-x-3 absolute hover:text-lime-400 transition"
           target="_blank"
           rel="noreferrer"
       >
+      <img src="../favicon.ico" className="h-6 mb-1 mr-1 inline" />
         AI Workouts
       </Link>
-      <Link href="https://github.com/cortez/workouts" className="top-3 right-4 space-x-3 absolute hover:text-gray-600"
+      <Link href="https://github.com/cortez/workouts" className="top-3 right-4 space-x-3 absolute hover:text-lime-400"
           target="_blank"
           rel="noreferrer"
       >
