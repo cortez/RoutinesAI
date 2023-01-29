2ee42272108ed472f9933351510e31b85b504036
cortez <joseph@cortez.link>
2023-01-28 22:42:50 -0600

    Committed with Git-Upload

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
 module.exports = {
   reactStrictMode: true,
 }
