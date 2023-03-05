59465896c826ec66e5ee880d0fe5963a52507383
cortez <joseph@cortez.link>
2023-03-04 23:45:40 -0600

    Committed with Git-Upload

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="mt-4 sm:ml-4 ml-3 px-3 py-1.5 bg-white text-black text-left">
-      <Link href="/" className="sm:font-medium text-lg absolute focus:outline-black"
+      <Link href="/" className="-mt-0.5 text-lg absolute focus:outline-black"
         target="_blank"
         rel="noreferrer"
       >
-        <img src="../favicon.ico" className="w-7 inline -mt-1 mr-2" />
+        <img src="../favicon.ico" className="w-6 inline -mt-1 mr-1.5" />
         Routines AI
       </Link>
-      <Link href="https://github.com/cortez/RoutinesAI" className="font-medium text-md absolute top-4 right-4 px-3 py-1.5 bg-gray-100 hover:bg-gray-200 active:scale-95 transition rounded-lg focus:outline-black"
+      <Link href="https://github.com/cortez/RoutinesAI" className="text-md absolute top-4 right-4 px-3 py-1.5 bg-gray-100 hover:bg-gray-200 active:scale-95 transition rounded-lg focus:outline-black"
           target="_blank"
           rel="noreferrer"
       >
