da12b2a3d156a2d44f7b804d452cc124a6c03705
Joseph Cortez <joseph@monogram.io>
2023-11-15 23:21:04 -0600

    updated theme

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,27 @@
-import Link from 'next/link'
+import Link from "next/link"
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center mb-10 lg:mb-14">
-      <Link href="/" className="-mt-0.5 text-lg focus:outline-black" target="_blank" rel="noreferrer">
-        <img src="../favicon.ico" className="w-6 inline -mt-1 mr-1.5" alt="Logo" />
+      <Link
+        href="/"
+        className="text-lg focus:outline-black"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="../favicon.ico"
+          className="w-6 inline -mt-1 mr-2"
+          alt="Logo"
+        />
         Routines AI
       </Link>
-      <Link href="https://github.com/cortez/RoutinesAI" className="text-md px-3 py-1.5 bg-[var(--gray)] hover:bg-[var(--gray-hover)] active:scale-95 transition-all rounded-xl focus:outline-black" target="_blank" rel="noreferrer">
+      <Link
+        href="https://github.com/cortez/RoutinesAI"
+        className="text-md px-3 py-1.5 bg-[var(--gray)] hover:bg-[var(--gray-hover)] active:scale-95 transition-all rounded-xl focus:outline-black"
+        target="_blank"
+        rel="noreferrer"
+      >
         GitHub
       </Link>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   future: {
-    hoverOnlyWhenSupported: true,
+    hoverOnlyWhenSupported: true
   },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
-        'blue': '#0B99FF'
-      },
-    },
+        blue: "#2D36FF"
+      }
+    }
   },
   plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")]
 }
